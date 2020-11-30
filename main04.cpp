86e1ee5a363faaf8a162b452ae7e955c0ba4beda
Garrett <13546043646@163.com>
2020-11-30 21:26:51 +0800

    不同路径

---
+++ main04.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int uniquePaths(int m, int n) {
+    //动态创建一个二维路径答案表
+    int** dp = (int**)malloc(sizeof(int*) * n);
+    for (int i = 0; i < n; i++) {
+        dp[i] = (int*)malloc(sizeof(int) * m);
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (i == 0 || j == 0) {     //最上一行或者最左一列
+                dp[i][j] = 1;
+            }
+            else {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+    }
+
+    return dp[n - 1][m - 1];
+}
+
