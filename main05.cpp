781bb9a1db0778dcddd970bc38abdd92944ac06f
Garrett <13546043646@163.com>
2020-12-13 13:01:01 +0800

    update

--- main05.cpp
+++ main05.cpp
@@ -12,6 +12,23 @@ struct ListNode {
 	ListNode(int x, ListNode* next) :val(x), next(next) {};
 };
 
+
+ ListNode* swapPairs(ListNode* head) {
+	         ListNode* dummyHead = new ListNode(0);
+		         dummyHead->next = head;
+			         ListNode* temp = dummyHead;
+				         while (temp->next != nullptr && temp->next->next != nullptr) {
+						             ListNode* node1 = temp->next;
+							                 ListNode* node2 = temp->next->next;
+									             temp->next = node2;
+										                 node1->next = node2->next;
+												             node2->next = node1;
+													                 temp = node1;
+															         }
+					         return dummyHead->next;
+						     }
+
+
 ListNode* swapPairs(ListNode* head) {
 	if (head == nullptr) {
 		return nullptr;
