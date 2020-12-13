8d1778f1d4c7da366bb4668fd88a35b1c6f4c0e4
Garrett <13546043646@163.com>
2020-12-13 15:42:07 +0800

    字典序的下一个序列

---
+++ main25.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void nextPermutation(vector<int>& nums) {
+	int len = nums.size();
+	if (len <= 1) {
+		return;
+	}
+	// 从后向前找,找到非降序的最后他一个元素
+	int i = len - 1;
+	while (i > 0 && nums[i - 1] >= nums[i]) {
+		i--;
+	}
+	// 若该序已经是最大字典序
+	if (i == 0) {
+		reverse(nums.begin(), nums.end());
+		return;
+	}
+	// 从后向前找，找到大于上步找到位置的元素
+	int j = len - 1;
+	while (j > 0 && nums[i - 1] >= nums[j]) {
+		j--;
+	}
+	// 将换该两个元素
+	swap(nums[i - 1], nums[j]);
+	// 将i - 1后面的元素进行翻转
+	reverse(nums.begin() + i, nums.end());
+}
+
+int main() {
+	vector<int> vec{ 1, 5, 1};
+	vector<int> vec1{ 3,2,1 };
+	nextPermutation(vec);
+	for (auto& ele : vec) {
+		cout << ele << endl;
+	}
+}
