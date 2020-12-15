17bd1d8f80c404c5c8f1b4ea7f45e1e85f126a86
Garrett <13546043646@163.com>
2020-12-15 09:17:44 +0800

    单调递增的数字

---
+++ main37.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+/*
+	单调递增的数字
+*/
+
+int monotoneIncreasingDigits(int N) {
+    string strN = to_string(N);
+    int i = 1;
+    // 直接使用前项递增的数字
+    while (i < strN.length() && strN[i - 1] <= strN[i]) {
+        i += 1;
+    }
+    // 若原数字中并不是递增的
+    if (i < strN.length()) {
+        // 从后往前数，递减的序列减一
+        while (i > 0 && strN[i - 1] > strN[i]) {
+            strN[i - 1] -= 1;
+            i -= 1;
+        }
+        // 出去前面递减的序列，其他位置皆为9
+        for (i += 1; i < strN.length(); ++i) {
+            strN[i] = '9';
+        }
+    }
+    // 字符串转数字
+    return stoi(strN);
+}
+
+int main() {
+    cout << monotoneIncreasingDigits(332) << endl;
+    return 0;
+}
